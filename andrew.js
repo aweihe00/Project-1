4bdf1ee01b9cda47423d9c19f83cb3704cdaf9fb
Andrew Weihe <andrewy03@gmail.com>
2019-12-07 13:50:05 -0600

    adding gmaps js logic

--- andrew.js
+++ andrew.js
@@ -1,1 +1,59 @@
-// start of my file
+var map
+
+function createMap () {
+  var options = {
+    center: { lat: 40.700610, lng: -73.997242, },
+    zoom: 14
+  };
+
+  map = new google.maps.Map(document.getElementById('map'), options);
+
+  var input = document.getElementById('search');
+  var searchBox = new google.maps.places.SearchBox(input);
+
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length === 0)
+      return;
+
+    markers.forEach(function (m) { m.setMap(null); });
+    markers = [];
+
+    var bounds = new google.maps.LatLngBounds();
+
+    places.forEach(function (p) {
+      if (!p.geometry)
+      return;
+    
+    markers.push(new google.maps.Marker({
+      map: map,
+      title: p.name,
+      position: p.geometry.location
+    }));
+
+    if (p.geometry.viewport)
+      bounds.union(p.geometry.viewport);
+    else
+      bounds.extend(p.geometry.location);
+  });
+  map.fitBounds(bounds);
+ });
+}
+
+function createMarker(options, html) {
+  var marker = new google.maps.Marker(options);
+  if (html) {
+    google.maps.event.addListener(marker, "click", function () {
+      infoWindow.setContent(html);
+      infoWindow.open(options.map, this);
+    });
+  }
+  return marker;
+}
